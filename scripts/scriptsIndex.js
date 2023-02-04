1ed4e2b810c18a397bf233fc1c4c37c326b21289
luisbelisario <luisbls77@hotmail.com>
2023-02-04 16:27:04 -0300

    finalizado background area e projects area

--- scripts/scriptsIndex.js
+++ scripts/scriptsIndex.js
@@ -1,7 +1,11 @@
 var langs = document.querySelector(".language-container")
-    link = document.querySelectorAll("a"),
+    link = document.querySelectorAll("a")
+    about = document.querySelector(".about")
+    projectsNav = document.querySelector(".projects-nav")
     introText = document.querySelector(".introText")
     btn = document.querySelector(".btn")
+    titleH1 = document.querySelector(".title-h1")
+    btnProjects = document.querySelector(".btn-projects")
 
 link.forEach(element => {
     element.addEventListener("click", () => {
@@ -10,25 +14,36 @@ link.forEach(element => {
 
         let attr = element.getAttribute("language")
 
+        about.textContent = data[attr].about
+        projectsNav.textContent = data[attr].projectsNav
         introText.textContent = data[attr].introText
         btn.textContent = data[attr].btn
+        titleH1.textContent = data[attr].titleH1
+        btnProjects.textContent = data[attr].btnProjects
     })
 });
 
 var data = {
     portuguese: {
+        about: "Sobre",
+        projectsNav: "Projetos",
         introText: "Desenvolvedor de software com ênfase em desenvolvimento back-end." +
         "Tenho experência de 2+ anos com Java | Springboot e outras tecnologias." +
         "Quer saber se posso contribuir com seu projeto? Clique no botão abaixo e saiba mais",
-        btn: "Saiba mais"
-
+        btn: "Saiba mais",
+        titleH1: "Meus projetos",
+        btnProjects: "Ver todos"
     },
     english: {
+        about: "About",
+        projectsNav: "Projects",
         introText: "Software developer with emphasis on" +
-        " backend development. I have experience of 2+ years" +
+        " back-end development. I have 2+ years experience" +
         " with Java | Springboot and other technologies." +
         " Want to know if I can contribute to your project?" +
         " Click the button below and learn more",
-        btn: "Click here"
+        btn: "Click here",
+        titleH1: "My projects",
+        btnProjects: "See all"
     }
 }
